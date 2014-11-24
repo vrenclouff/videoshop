bb0e5b67d19e4090aa45197b6a3eee614d625c3f
Lukas <luc.cerny@gmail.com>
2014-11-24 22:37:54 +0000

    Rozbrazeni profilu uzivatele

---
+++ controllers/ProfilController.php
@@ -0,0 +1,38 @@
+<?php
+
+class ProfilController extends AbsController
+{
+
+    public function make($param){
+
+//        print_r($_SESSION['user_profil']);
+//        echo "<br /><br />";
+        extract($_SESSION['user_profil']);
+
+        $this->data = array(
+            'title' => 'Půjčovna filmů',
+            'PasswordOld' => 'Stare heslo',
+            'PasswordOldAgain' => 'Stare heslo znovu',
+            'PasswordNew' => 'Nove heslo',
+            'FName_name' => 'Křestní jméno *',
+            'LName_name' => 'Příjmení *',
+            'Tel_name' => 'Telefon',
+            'City_name' => 'Město',
+            'PSC_name' => 'PSC',
+            'Street_name' => 'Ulice',
+            'fname' => $fname,
+            'lname' => $lname,
+            'tel' => $tel,
+            'city' => $mesto,
+            'psc' => $psc,
+            'street' => $ulice
+        );
+
+        $this->temp = 'profil';
+        $this->view();
+
+    }
+
+}
+
+?>
